aabbe58ca296e609cb1d849f1fe676c3805b7d59
Michael <shadesofmike@gmail.com>
2015-06-20 10:46:10 +0800

    Strobe toggle

--- SyncoStrobe/ViewController.swift
+++ SyncoStrobe/ViewController.swift
@@ -11,11 +11,17 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
+    var timer = NSTimer()
+
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+      
         
-        print(strobe())
+        
+        strobeOn()
         // prints "hello, world"
 
         
@@ -30,19 +36,23 @@ class ViewController: UIViewController {
 }
 
 
-func strobe() -> String {
-  
+
+
+func strobeOn() -> String {
+
     
-    let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
-    if device.hasTorch {
-        device.lockForConfiguration(nil)
+let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+if (device.hasTorch) {
+    
+    println("device has touch")
+    
+    device.lockForConfiguration(nil)
+    if (device.torchMode == AVCaptureTorchMode.On) {
+        device.torchMode = AVCaptureTorchMode.Off
+    } else {
         device.setTorchModeOnWithLevel(1.0, error: nil)
-        device.unlockForConfiguration()
     }
-    
-    println("Hi!")
-    
-    return "Strobe"
+    device.unlockForConfiguration()
 }
 
-
+}
