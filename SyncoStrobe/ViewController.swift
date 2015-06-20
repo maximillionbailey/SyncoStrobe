8d1de19c1ec9d5763a655e55724701a9ee8ad125
Michael <shadesofmike@gmail.com>
2015-06-20 08:40:57 +0800

    Initial Commit

---
+++ SyncoStrobe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SyncoStrobe
+//
+//  Created by Michael Bailey on 20/06/2015.
+//  Copyright (c) 2015 Bailey's Empire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
